53bf29184151a81ea0c1a337d3adca5857223daa
shivasurya <shivasurya@users.noreply.github.com>
2025-11-25 03:06:49 +0000

    Update pathfinder to 0.0.34

--- Formula/pathfinder.rb
+++ Formula/pathfinder.rb
@@ -4,7 +4,7 @@ class Pathfinder < Formula
   desc "Open-source security suite with structural code analysis and AI-powered vulnerability detection"
   homepage "https://codepathfinder.dev/"
   license "AGPL-3.0-only"
-  version "0.0.34-rc1"
+  version "0.0.34"
 
   # Platform-specific binary downloads
   # NOTE: Each tarball contains a file named "pathfinder"
@@ -12,22 +12,22 @@ class Pathfinder < Formula
   on_macos do
     on_arm do
       url "https://github.com/shivasurya/code-pathfinder/releases/download/v#{version}/pathfinder-darwin-arm64.tar.gz"
-      sha256 "3e22610931ce4d0bd95914cb4f691c1374311c381a49342bbca7ddf8b9181062" # darwin-arm64
+      sha256 "d6c047b6e3ec6c0ef604b2cdaf0eecc8c8ff51de8d9e2a6a28383e3546a90c57" # darwin-arm64
     end
     on_intel do
       url "https://github.com/shivasurya/code-pathfinder/releases/download/v#{version}/pathfinder-darwin-amd64.tar.gz"
-      sha256 "2979dbd73efc9c8fed4f1dabd6327baff98ab0e04176d9f175f2bbcaf4963b0b" # darwin-amd64
+      sha256 "8151bde1b98818369be29605682f81ee3164f31f1261e477d49560e36035590d" # darwin-amd64
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/shivasurya/code-pathfinder/releases/download/v#{version}/pathfinder-linux-arm64.tar.gz"
-      sha256 "f75b81efc1d095b0c9e9a232581e07a89ea2ec5adb2668e0082d852d956f11c8" # linux-arm64
+      sha256 "57963000dc2a40b64ddc80a0dc595316e43eaf715e933194ca53f90a1c9b5e86" # linux-arm64
     end
     on_intel do
       url "https://github.com/shivasurya/code-pathfinder/releases/download/v#{version}/pathfinder-linux-amd64.tar.gz"
-      sha256 "d360c4f2d2f5467a32f385df00d08a5d7abac454f5e4cd946d79b311e68d7ce4" # linux-amd64
+      sha256 "08b86d3d9254c4bdd5b9c130f6cdfe569fde07ca19fde8e4a186e4f7b9398942" # linux-amd64
     end
   end
 
